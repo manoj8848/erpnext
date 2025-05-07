1264efc6281a9f602816505e3152c6391852545f
hrishikesh8848 <158465727+hrishikesh8848@users.noreply.github.com>
2025-05-07 10:35:47 +0530

    Update test_request_for_quotation.py

--- erpnext/buying/doctype/request_for_quotation/test_request_for_quotation.py
+++ erpnext/buying/doctype/request_for_quotation/test_request_for_quotation.py
@@ -213,7 +213,7 @@ class TestRequestforQuotation(FrappeTestCase):
 		self.assertIn("Dear", preview)
 		self.assertIn("testrfquser@example.com", preview)
 
-def test_get_supplier_tag_returns_list(self):
+def test_get_supplier_tag(self):
         from erpnext.buying.doctype.request_for_quotation.request_for_quotation import get_supplier_tag
 		tags = get_supplier_tag()
         self.assertIsInstance(tags, list)
