8a636df0b0b3210d6576613d6f7fe4de61e509db
patel_asif <aasif@8848digital.com>
2025-05-19 19:06:05 +0530

    fix: Hotfix the inconsistent indentation issue

--- erpnext/accounts/doctype/process_statement_of_accounts/process_statement_of_accounts.py
+++ erpnext/accounts/doctype/process_statement_of_accounts/process_statement_of_accounts.py
@@ -322,10 +322,8 @@ def get_recipients_and_cc(customer, doc):
 	for clist in doc.customers:
 		if clist.customer == customer:
 			if clist.billing_email:
-				if clist.billing_email:
- 				for email in clist.billing_email.split(","):
- 					recipients.append(email.strip())
-					
+				for email in clist.billing_email.split(","):
+					recipients.append(email.strip())
 			if doc.primary_mandatory and clist.primary_email:
 				for email in clist.primary_email.split(","):
 					recipients.append(email.strip())
